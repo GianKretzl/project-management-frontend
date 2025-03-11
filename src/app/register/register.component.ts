080805fb1c69d4248d9ce8c9d2cdbc84b9ebf735
GianKretzl <gian.kretzl@outlook.com>
2025-03-11 13:27:31 -0300

    fix register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
+  standalone: true,
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  imports: [FormsModule]
 })
 export class RegisterComponent {
   username: string = '';
