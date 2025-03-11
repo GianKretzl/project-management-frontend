b74dfbaa6024e764541b9d80cb48fb8961cd8214
Gian Kretzl <gian.kretzl@outlook.com>
2025-03-11 12:15:40 -0300

    fix login and register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,28 +1,11 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
+  imports: [],
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  username: string = '';
-  email: string = '';
-  password: string = '';
-  role: string = 'USER';
 
-  constructor(private router: Router) {}
-
-  onSubmit() {
-    // Simulação de registro de usuário
-    console.log('Usuário registrado com sucesso:', {
-      username: this.username,
-      email: this.email,
-      password: this.password,
-      role: this.role
-    });
-    alert('Cadastro realizado com sucesso!');
-    this.router.navigate(['/login']);
-  }
 }
